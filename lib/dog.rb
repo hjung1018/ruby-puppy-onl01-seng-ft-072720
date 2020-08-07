302c4c05b4354c7f7da2a50298c85f46bed4a791
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-07 11:52:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,39 @@
 # Add your code here
+class Dog
+@@all = []
+
+attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+def save 
+  @@all << 
+end
+
+def self.all
+  @@all
+end
+
+def self.clear_all
+  @@all.clear
+end
+
+def print_all
+   @@all.each do |dog|
+   print "#{dog} /n"
+   
+end
+end
+
+
+end
+
+  # describe ".print_all" do
+  #     it "is a class method that puts out the name of each dog to the terminal" do
+  #       expect{Dog.print_all}.to output("Pluto\nFido\nMaddy\n").to_stdout
+  #     end
+  #   end
+  # end
